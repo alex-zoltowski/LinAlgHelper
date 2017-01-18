b4e5e089488f85d9d90871a848c1cc9fe202187d
Alex Zoltowski <zolto1ap@cmich.edu>
2017-01-18 02:04:37 -0500

    scale function

--- LinAlgHelper.py
+++ LinAlgHelper.py
@@ -21,59 +21,86 @@ class AugmentedMatrix():
 
         return 0
 
+    def get_leading_num(self, row):
+        return self.matrix[row][self.get_leading_col(row)]
+
+    def floats_to_ints(self):
+        for i in range(self.rows):
+            for x in range(self.cols):
+                if float(self.matrix[i][x]).is_integer():
+                    self.matrix[i][x] = int(self.matrix[i][x])
+
     def print_matrix(self):
-        print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+        print('\n'.join([''.join(['{:5}'.format(item) for item in row])
             for row in self.matrix]))
 
         print('----------------------')
 
     def swap(self, row1, row2):
         for i in range(self.cols):
-            self.matrix[row1][i] = self.matrix[row2][i]
-            self.matrix[row2][i] = self.matrix[row1][i]
+            num1 = self.matrix[row1][i]
+            num2 = self.matrix[row2][i]
+            self.matrix[row1][i] = num2
+            self.matrix[row2][i] = num1
 
         self.print_matrix()
 
     def replace(self, change_row, base_row):
-        factor = self.matrix[change_row][self.get_leading_col(change_row)] * -1
+        factor = (self.matrix[change_row][self.get_leading_col(change_row)] * -1) \
+                    / (self.matrix[base_row][self.get_leading_col(base_row)])
         for i in range(self.cols):
             self.matrix[change_row][i] += factor * self.matrix[base_row][i]
 
+        self.floats_to_ints()
         self.print_matrix()
 
     def scale(self):
         for row in range(self.rows):
-            divisor = reduce(lambda x,y: gcd(x, y), self.matrix[row])
-            if divisor != 1 and len(set(self.matrix[row])) > 2:
-                print(divisor)
-                for i in range(self.cols):
-                    print(self.matrix[row][i])
-                    self.matrix[row][i] /= divisor
-            if len(set(self.matrix[row])) == 2:
-                for i in range(self.cols):
-                    if self.matrix[row][i] != 0:
-                        self.matrix[row][i] = 1
+            if self.get_leading_num(row) < 1.0 and\
+                    self.get_leading_num(row) > -1.0:
+                divisor = self.get_leading_num(row)
+            elif self.get_leading_num(row) == 0 or\
+                    self.get_leading_num(row) == 1:
+                continue
+            else:
+                divisor = abs(reduce(lambda x,y: gcd(x, y), self.matrix[row]))
+                if self.get_leading_num(row) < 0:
+                    divisor *= -1
 
-            self.print_matrix()
+            for i in range(self.cols):
+                self.matrix[row][i] /= divisor
+
+        self.floats_to_ints()
+        self.print_matrix()
+
+    def get_echelon_form(self):
+        matrix = self.matrix
+        matrix.scale()
+        working_column = 0
+        # for i in range(self.rows):
+        #     if matrix[i][working_column] == 1 and i < :
+
+        return matrix
+
 
 
 matrix = AugmentedMatrix()
-print(-8*(1/2))
-matrix.set_val(0, 0, 1)
-matrix.set_val(0, 1, -2)
-matrix.set_val(0, 2, 1)
-matrix.set_val(0, 3, 0)
-matrix.set_val(1, 0, 0)
-matrix.set_val(1, 1, 2)
-matrix.set_val(1, 2, -8)
-matrix.set_val(1, 3, 8)
-matrix.set_val(2, 0, 5)
-matrix.set_val(2, 1, 0)
-matrix.set_val(2, 2, -5)
-matrix.set_val(2, 3, 10)
 
+matrix.set_val(0, 0, 1)
+matrix.set_val(0, 1, 2)
+matrix.set_val(0, 2, 4)
+matrix.set_val(0, 3, 2)
+matrix.set_val(1, 0, 0)
+matrix.set_val(1, 1, -6)
+matrix.set_val(1, 2, 2)
+matrix.set_val(1, 3, 4)
 matrix.print_matrix()
-matrix.replace(2, 0)
 matrix.scale()
-matrix.replace(2, 1)
-matrix.scale()
+# matrix.set_val(2, 0, 4)
+# matrix.set_val(2, 1, -8)
+# matrix.set_val(2, 2, 12)
+# matrix.set_val(2, 3, 1)
+#
+# matrix.print_matrix()
+# matrix.swap(0, 1)
+# matrix.replace(2, 0)
